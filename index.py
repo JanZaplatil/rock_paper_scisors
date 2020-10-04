3e9059819282f687bdf048a03c901fde80c9e20b
JanZaplatil <30730161+JanZaplatil@users.noreply.github.com>
2020-10-04 13:36:51 +0200

    Update index.py

--- index.py
+++ index.py
@@ -1,4 +1,6 @@
+#!/usr/bin/env python3
 import Tkinter
+from Tkinter import *
 import tkMessageBox
 import random
 
@@ -12,44 +14,40 @@ greeting.pack()
 guess = Tkinter.Entry(window)
 guess.pack()
 
+my_list = ["rock","paper","scisors"]
+
+def game():
+    my_choice = guess.get()
+    com_choice = my_list[random.randint(0,2)]
+    
+    if my_choice == "rock":
+        if my_choice == com_choice:
+            result_text = " you both chose rock"
+        elif com_choice == "paper":
+            result_text = "you chose rock and comp paper"
+        elif com_choice == "scisors":
+            result_text = "you chose rock and comp scisors"
+
+    if my_choice == "paper":
+        if my_choice == com_choice:
+            result_text = " you both chose paper "
+        elif com_choice == "rock":
+            result_text = "you chose paper and comp rock"
+        elif com_choice == "scisors":
+            result_text = "you chose paper and comp scisors"
+    
+    if my_choice == "scisors":
+        if my_choice == com_choice:
+            result_text = " you both chose scisors "
+        elif com_choice == "rock":
+            result_text = "you chose scisors and comp rock"
+        elif com_choice == "paper":
+            result_text = "you chose scisors and comp paper"
+
+    tkMessageBox.showinfo("Result", result_text)
+
+submit = Tkinter.Button(window, text="Submit", command=game)  # check_guess, not check_guess()
+submit.pack()
 
 while True:
-    myList = ["rock","paper","scisors"]
-
-    myChoice = guess.get()
-    comChoice = myList[random.randint(0,2)]
-
-
-    def game():
-        global result_text
-        if guess.get() == "rock":
-            if guess.get() == comChoice:
-                result_text = " you both chose rock"
-            elif comChoice == "paper":
-                result_text = "you chose rock and comp paper"
-            elif comChoice == "scisors":
-                result_text = "you chose rock and com scisors"
-
-        if guess.get() == "paper":
-            if guess.get() == comChoice:
-                result_text = " you both chose paper "
-            elif comChoice == "rock":
-                result_text = "you chose paper and comp paper"
-            elif comChoice == "scisors":
-                result_text = "you chose paper and com scisors"
-        
-        if guess.get() == "scisors":
-            if guess.get() == comChoice:
-                result_text = " you both chose scisors "
-            elif comChoice == "rock":
-                result_text = "you chose scisors and comp rock"
-            elif comChoice == "paper":
-                result_text = "you chose scisors and com paper"
-
-        tkMessageBox.showinfo("Result", result_text)
-
-    # submit button
-    submit = Tkinter.Button(window, text="Submit", command=game)  # check_guess, not check_guess()
-    submit.pack()
-
     window.mainloop()
